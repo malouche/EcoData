938346629c67b7c6d27eff72cad5436e7682bd17
dhafer malouche <dhafer.malouche@essai.u-carthage.tn>
2024-11-06 13:48:43 +0300

    ok

--- contact_info.py
+++ contact_info.py
@@ -19,15 +19,15 @@ def show_contact_info():
     st.write("""
         This application is designed to facilitate accessing two types of datasets:
 
-            1. **Ecological & Biological Data**: Most of these datasets are from the book: *Biostatistical Design and Analysis Using R: A Practical Guide* by Murray Logan (2010), Wiley-Blackwell. ISBN: 978-1-405-19008-4. These datasets cover various ecological studies including beetle populations, ant biomass, rodent species, and more.
+        **Ecological & Biological Data**: Most of these datasets are from the book: *Biostatistical Design and Analysis Using R: A Practical Guide* by Murray Logan (2010), Wiley-Blackwell. ISBN: 978-1-405-19008-4. These datasets cover various ecological studies including beetle populations, ant biomass, rodent species, and more.
 
-            2. **AQUASTAT Data**: AQUASTAT is FAO's (Food and Agriculture Organization) global water information system, providing comprehensive data on:
-                     - Water resources and their use
-                     - Agricultural water management
-                     - Water and food security
-                    - Environmental sustainability
+        **AQUASTAT Data**: AQUASTAT is FAO's (Food and Agriculture Organization) global water information system, providing comprehensive data on:
+            - Water resources and their use
+            - Agricultural water management
+            - Water and food security
+            - Environmental sustainability
 
             
-            For any questions or suggestions about this app's functionality, please don't hesitate to reach out through any of the channels above.
+        For any questions or suggestions about this app's functionality, please don't hesitate to reach out through any of the channels above.
 
     """)
